29d3ea96fc0c5665edc77120f9eeed34fed23a23
medme <ge47wog@mytum.de>
2022-06-04 14:41:37 +0200

    created a basic skeleton for the project

---
+++ src/server/java/de/changefans/ChangeFansServerApplication.java
@@ -0,0 +1,13 @@
+package de.changefans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChangeFansServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChangeFansServerApplication.class, args);
+    }
+
+}
