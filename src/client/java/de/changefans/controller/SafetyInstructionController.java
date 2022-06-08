bc3ae3dd37d36100d12d1164d9c288a1e11a4ba2
medme <ge47wog@mytum.de>
2022-06-08 22:34:13 +0200

    added safetyInstruction Resource

--- src/client/java/de/changefans/controller/SafetyInstructionController.java
+++ src/client/java/de/changefans/controller/SafetyInstructionController.java
@@ -1,4 +1,36 @@
 package de.changefans.controller;
 
+import de.changefans.model.SafetyInstruction;
+import javafx.scene.image.Image;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+
+import java.io.ByteArrayInputStream;
+import java.util.function.Consumer;
+
 public class SafetyInstructionController {
+
+    private final WebClient webClient;
+
+    public SafetyInstructionController() {
+        this.webClient = WebClient.builder()
+                .baseUrl("http://localhost:8080/")
+                .defaultHeader(HttpHeaders.ACCEPT, MediaType.IMAGE_JPEG_VALUE)
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.IMAGE_JPEG_VALUE)
+                .build();
+    }
+
+    public void getSafetyInstruction(String type, Consumer<SafetyInstruction> imageConsumer) {
+        byte[] imageBytes = webClient.get()
+                .uri(uriBuilder -> uriBuilder.path("safetyInstruction")
+                        .queryParam("type", type).build())
+                .retrieve()
+                .bodyToMono(byte[].class)
+                .onErrorStop()
+                .block();
+        assert imageBytes != null;
+        imageConsumer.accept(new SafetyInstruction(type, new Image(new ByteArrayInputStream(imageBytes))));
+    }
 }
--- src/server/java/de/changefans/rest/SafetyInstructionResource.java
+++ src/server/java/de/changefans/rest/SafetyInstructionResource.java
@@ -1,4 +1,36 @@
 package de.changefans.rest;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+@RestController
+@RequestMapping(consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = {MediaType.IMAGE_JPEG_VALUE})
+
 public class SafetyInstructionResource {
+    @GetMapping("safetyInstruction")
+    public ResponseEntity<byte[]> getSafetyInstruction(@RequestParam("type") boolean detailed) {
+        String imagePath;
+        if (detailed) {
+            imagePath = "/resources/SafetyInstructions/detailedSafetyInstructions.jpg";
+        } else {
+            imagePath = "/resources/SafetyInstructions/simpleSafetyInstructions.jpg";
+        }
+        File imageFile = new File(imagePath);
+        try {
+            byte[] image = Files.readAllBytes(imageFile.toPath());
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.IMAGE_JPEG);
+            headers.setContentLength(image.length);
+            return new ResponseEntity<>(image, headers, HttpStatus.OK);
+        } catch (IOException e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
 }
