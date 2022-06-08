97487c8d6d6d0314b18a0d703a3c0e293a4f3d0c
medme <ge47wog@mytum.de>
2022-06-08 22:34:13 +0200

    added safetyInstruction Resource

--- src/common/java/de/changefans/model/SafetyInstruction.java
+++ src/common/java/de/changefans/model/SafetyInstruction.java
@@ -1,4 +1,22 @@
 package de.changefans.model;
 
+import javafx.scene.image.Image;
+
 public class SafetyInstruction {
+
+    private boolean detailed;
+    private Image image;
+
+    public SafetyInstruction(String type, Image image) {
+        this.image = image;
+        detailed = type.equals("detailed");
+    }
+
+    public boolean isDetailed() {
+        return detailed;
+    }
+
+    public Image getImage() {
+        return image;
+    }
 }
