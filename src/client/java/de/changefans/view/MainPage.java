ff20db661d63b55c4f922a98c809ad5615f1695a
Aminehd <68565622+AmineHadd@users.noreply.github.com>
2022-06-29 20:37:31 +0100

    interfacedc

--- src/client/java/de/changefans/view/MainPage.java
+++ src/client/java/de/changefans/view/MainPage.java
@@ -1,4 +1,17 @@
 package de.changefans.view;
 
+import javafx.scene.input.MouseEvent;
+
 public class MainPage {
+    public void FlightInformationClicked(MouseEvent mouseEvent) {
+    }
+
+    public void SafetyInstrucionPartClicked(MouseEvent mouseEvent) {
+    }
+
+    public void RequestServiceClicked(MouseEvent mouseEvent) {
+    }
+
+    public void MainPageClicked(MouseEvent mouseEvent) {
+    }
 }
