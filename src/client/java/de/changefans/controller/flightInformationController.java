e8de57fbaf40a8fe141f2c82fd428caa7153e660
Aminehd <68565622+AmineHadd@users.noreply.github.com>
2022-07-20 23:14:44 +0200

    interfacedc

--- src/client/java/de/changefans/controller/flightInformationController.java
+++ src/client/java/de/changefans/controller/flightInformationController.java
@@ -1,4 +1,14 @@
 package de.changefans.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
 public class flightInformationController {
+
+    @FXML
+    private TextArea departureBox;
+
+    @FXML
+    private TextArea landingBox;
+
 }
