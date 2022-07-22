87562506d7aa453c645453842682c839144cd059
medme <ge47wog@mytum.de>
2022-07-22 16:26:36 +0200

    java docs

---
+++ package-search-index.js
@@ -0,0 +1,5 @@
+packageSearchIndex = [{
+    "l": "All Packages",
+    "u": "allpackages-index.html"
+}, {"l": "de.changefans"}, {"l": "de.changefans.controller"}, {"l": "de.changefans.model"}, {"l": "de.changefans.rest"}, {"l": "de.changefans.service"}, {"l": "de.changefans.view"}];
+updateSearchResults();
